6ef7be0a27b119c4272014efcf6a2f2c108241ae
Rohit Sharma <rohit.aproperemail@gmail.com>
2023-01-11 10:34:20 +0530

    student-class-added

--- Git_Project1/src/main/java/com/cg/MainApplication.java
+++ Git_Project1/src/main/java/com/cg/MainApplication.java
@@ -3,5 +3,6 @@ package com.cg;
 public class MainApplication {
 	public static void main(String[] args) {
 		System.out.println("-- this is for git repo --");
+		System.out.println("-- unstaged changes --");
 	}
 }
